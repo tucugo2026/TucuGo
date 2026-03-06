bd1e89ed7b3e86c16da87f6d512efd826629028d
tucugo2026 <tucugo2026@gmail.com>
2026-03-06 20:22:42 -0300

    Initialize Firebase app and export services

--- admin_web/firebase.js
+++ admin_web/firebase.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBVu53drRzUbLSIYxdaGlxYK63JrSWIYxc",
+  authDomain: "tucugo-37d6c.firebaseapp.com",
+  projectId: "tucugo-37d6c",
+  storageBucket: "tucugo-37d6c.firebasestorage.app",
+  messagingSenderId: "156717270913",
+  appId: "1:156717270913:web:1a57d2f998d7b5983a8a80"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
